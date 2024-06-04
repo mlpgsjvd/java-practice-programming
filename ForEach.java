922da86aa1f0114944e4b40311ca18f69ba730b8
Pallavi S <115915060+mlpgsjvd@users.noreply.github.com>
2024-06-04 16:39:41 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class ForEach {
+
+
+		public static void main(String[] args) {
+			int[]  a= {1,2,3};
+			for(int i=0;i<a.length;i++)
+			{
+				System.out.print(a[i]+" ");
+			}
+			System.out.println("\n-----");
+			for(int i:a)
+			{
+				System.out.print(i+" ");
+			}
+
+		}
+
+	}
